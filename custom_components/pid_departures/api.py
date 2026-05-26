a83cda625da11fb848b26dab6a6f16da4f1432ab
Martin Góč <martingaming08@gmail.com>
2026-05-26 11:35:26 +0200

    api debug

--- custom_components/pid_departures/api.py
+++ custom_components/pid_departures/api.py
@@ -1,6 +1,9 @@
 import aiohttp
+import logging
 
 from .const import API_BASE
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class PIDDeparturesApi:
@@ -22,13 +25,32 @@ class PIDDeparturesApi:
             f"&minutesBefore=0"
             f"&minutesAfter=180"
         )
-    
+
         async with aiohttp.ClientSession() as session:
             async with session.get(url, headers=self.headers) as response:
-                response.raise_for_status()
-                data = await response.json()
-    
+
+                text = await response.text()
+
+                if response.status != 200:
+                    _LOGGER.error(
+                        "Golemio API error %s\nURL: %s\nResponse body: %s",
+                        response.status,
+                        url,
+                        text,
+                    )
+                    return {}
+
+                try:
+                    data = await response.json()
+                except Exception:
+                    _LOGGER.error(
+                        "Invalid JSON response\nURL: %s\nBody: %s",
+                        url,
+                        text,
+                    )
+                    return {}
+
                 if isinstance(data, list):
                     return data[0] if data else {}
-    
+
                 return data or {}
