db9d58496963925db340e8a353432285a09ecaff
Martin Góč <martingaming08@gmail.com>
2026-05-26 10:42:16 +0200

    config flow fix

--- custom_components/pid_departures/config_flow.py
+++ custom_components/pid_departures/config_flow.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 import voluptuous as vol
+import logging
 
 from homeassistant import config_entries
 from homeassistant.helpers import storage
 
 from .api import PIDDeparturesApi
 from .const import CONF_API_KEY, CONF_STOP_ID, DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 STORAGE_KEY = "pid_departures_global"
 STORAGE_VERSION = 1
@@ -41,7 +44,8 @@ class PIDDeparturesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             try:
                 data = await api.get_stop_departures(stop_id)
-            except Exception:
+            except Exception as err:
+                _LOGGER.error("Cannot connect to Golemio API: %s", err)
                 return self.async_show_form(
                     step_id="user",
                     errors={"base": "cannot_connect"},
@@ -50,15 +54,24 @@ class PIDDeparturesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             stop_name = "Unknown stop"
 
-            if data:
+            if isinstance(data, dict):
+                stop = data.get("stop", {})
+                if isinstance(stop, dict):
+                    stop_name = stop.get("name", stop_name)
+
+            elif isinstance(data, list) and len(data) > 0:
                 stop = data[0].get("stop", {})
-                stop_name = stop.get("name", stop_name)
+                if isinstance(stop, dict):
+                    stop_name = stop.get("name", stop_name)
+
+            if not stop_name:
+                stop_name = stop_id
 
             await self.async_set_unique_id(stop_id)
             self._abort_if_unique_id_configured()
 
             return self.async_create_entry(
-                title=stop_name,
+                title=f"{stop_name}",
                 data={
                     CONF_STOP_ID: stop_id
                 }
